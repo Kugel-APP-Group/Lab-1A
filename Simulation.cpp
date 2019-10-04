cbc65546fe919f9e4139221f2f3b7db11057c265
Edward <edward.shen.sdn@gmail.com>
2019-10-03 22:32:27 -0400

    Added Titles

--- Simulation.cpp
+++ Simulation.cpp
@@ -58,6 +58,7 @@ void step(){
 }
 int main(){
     freopen("//Users//kevinwan//Desktop//sim1.csv","w",stdout);
+    printf("s1.x,s1.y,s2.x,s2.y,v1.x,v1.y,v2.x,v2.y,p1.x,p1.y,p2.x,p2.y,L1,L2,L,E1,E2,E\n");
     for(int i=0;i<10000;i++){
         step();
         printf("%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n",s1.x,s1.y,s2.x,s2.y,v1.x,v1.y,v2.x,v2.y,p1.x,p1.y,p2.x,p2.y,L1,L2,L,E1,E2,E);
